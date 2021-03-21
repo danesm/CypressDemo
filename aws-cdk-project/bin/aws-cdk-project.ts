0a43535412b1f6df1671a7878f5e32b49629bd77
Mishra <mishdane@147dda0f005b.ant.amazon.com>
2021-03-21 15:08:33 +0000

    Updated for using multiple profiles

--- aws-cdk-project/bin/aws-cdk-project.ts
+++ aws-cdk-project/bin/aws-cdk-project.ts
@@ -4,4 +4,22 @@ import * as cdk from '@aws-cdk/core';
 import { AwsCdkProjectStack } from '../lib/aws-cdk-project-stack';
 
 const app = new cdk.App();
-new AwsCdkProjectStack(app, 'AwsCdkProjectStack');
+
+
+new AwsCdkProjectStack(app, 'NovartisTest',{env: { account: '767322885366', region: 'us-east-1' }});
+new AwsCdkProjectStack(app, 'NovartisDev',{env: { account: '211826912675', region: 'eu-west-2' }});
+
+
+
+
+
+//new AwsCdkProjectStack(app, 'AwsCdkProjectStack');
+
+
+/*
+multiple stacks with the same name in different accounts/regions. Consider following example code:
+
+const app = new cdk.App()
+new cdk.Stack(app, 'test1', { stackName: 'test', env: { account: 'XXXXXXXXXXXX', region: 'eu-west-1' } })
+new cdk.Stack(app, 'test2', { stackName: 'test', env: { account: 'XXXXXXXXXXXX', region: 'us-east-1' } })
+*/
--- aws-cdk-project/lib/aws-cdk-project-stack.ts
+++ aws-cdk-project/lib/aws-cdk-project-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deploy from '@aws-cdk/aws-s3-deployment';
-
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import { Duration } from '@aws-cdk/core';
 
 
 export class AwsCdkProjectStack extends cdk.Stack {
@@ -11,20 +11,25 @@ export class AwsCdkProjectStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const bucket_name = this.node.tryGetContext('bucket_name');
+
     //create s3 bucket
 
-    const dbmBucket = new s3.Bucket(this,'dbm-testbucket-20210217', {
-                  publicReadAccess: true,
-                  websiteIndexDocument: 'index.html'
+    //const dbmBucket = new s3.Bucket(this,'dbm-testbucket-20210217', {
+      const dbmBucket = new s3.Bucket(this, bucket_name, {
+                  publicReadAccess: false,
+                  websiteIndexDocument: 'index.html',
+                  versioned: false
+
 
     });
 
    // Deploy code from build folder to s3.
 
-
-   new s3Deploy.BucketDeployment(this, 'dbm-testbucketDeploy-20210218', {
+   new s3Deploy.BucketDeployment(this, 'dbmbucketDeploy-20210218', {
     sources: [s3Deploy.Source.asset('../build')],
-    destinationBucket: dbmBucket
+    destinationBucket: dbmBucket,
+    cacheControl:[s3Deploy.CacheControl.maxAge(Duration.seconds(60))]   
     //destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
   });
 
