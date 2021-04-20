645a3dbfa58ff6fbe8581562c0f5c26170dc3ac3
Mishra <mishdane@147dda0f005b.ant.amazon.com>
2021-04-20 09:13:54 +0100

    Removed deprecated lib allowed-ip-waf

--- aws-cdk-project/lib/dev-stack.ts
+++ aws-cdk-project/lib/dev-stack.ts
@@ -3,7 +3,7 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import { Duration } from "@aws-cdk/core";
-import { AllowedIPsWaf, IPScope, IPv4 } from "allowed-ips-waf";
+import * as wafv2 from "@aws-cdk/aws-wafv2";
 
 export class DevStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -39,9 +39,75 @@ export class DevStack extends cdk.Stack {
     const whitelistIP1 = this.node.tryGetContext("dev_whitelist_ip1");
     const whitelistIP2 = this.node.tryGetContext("dev_whitelist_ip2");
 
-    const allowedIPsWaf = new AllowedIPsWaf(this, "AllowedIPsWaf", {
-      ipScope: IPScope.CLOUDFRONT,
-      allowedIPs: [new IPv4(whitelistIP1), new IPv4(whitelistIP2)],
+    const webAcl = new wafv2.CfnWebACL(this, "WebAcl", {
+      defaultAction: { allow: {} },
+      description: "LEAP PnC Web ACL",
+      tags: [{ key: "LEAP", value: "Web ACL for LEAP PnC" }],
+      rules: [
+        {
+          priority: 1,
+          overrideAction: { none: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "AWS-AWSManagedRulesAmazonIpReputationList",
+          },
+          name: "AWS-AWSManagedRulesAmazonIpReputationList",
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: "AWS",
+              name: "AWSManagedRulesAmazonIpReputationList",
+            },
+          },
+        },
+        {
+          priority: 2,
+          overrideAction: { none: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "AWS-AWSManagedRulesCommonRuleSet",
+          },
+          name: "AWS-AWSManagedRulesCommonRuleSet",
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: "AWS",
+              name: "AWSManagedRulesCommonRuleSet",
+            },
+          },
+        },
+        {
+          priority: 3,
+          overrideAction: { none: {} },
+          visibilityConfig: {
+            sampledRequestsEnabled: true,
+            cloudWatchMetricsEnabled: true,
+            metricName: "AWS-AWSManagedRulesKnownBadInputsRuleSet",
+          },
+          name: "AWS-AWSManagedRulesKnownBadInputsRuleSet",
+          statement: {
+            managedRuleGroupStatement: {
+              vendorName: "AWS",
+              name: "AWSManagedRulesKnownBadInputsRuleSet",
+            },
+          },
+        },
+      ],
+      scope: "CLOUDFRONT",
+      visibilityConfig: {
+        sampledRequestsEnabled: true,
+        cloudWatchMetricsEnabled: true,
+        metricName: "web-acl",
+      },
+    });
+
+    //IpRule-Allow list
+    new wafv2.CfnIPSet(this, "LEAP_IPrule", {
+      addresses: [whitelistIP1, whitelistIP2],
+      ipAddressVersion: "IPV4",
+      scope: "CLOUDFRONT",
+      description: "LEAP PnC IP Allow List",
+      tags: [{ key: "LEAP", value: "IPSet for LEAP PnC" }],
     });
 
     //Create new origin access identity - a cloudfront user
@@ -66,7 +132,7 @@ export class DevStack extends cdk.Stack {
           },
         ],
         //Implements Ip Whitelisting.
-        webACLId: allowedIPsWaf.webACLId,
+        webACLId: webAcl.attrArn,
         //Enable cloudfront access log (standard logging) with a default logging bucket.
         loggingConfig: {},
       }
